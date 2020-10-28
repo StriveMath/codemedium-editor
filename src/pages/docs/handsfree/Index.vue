791bfe752231729ea1e91ef6484b24e6a43d4f52
Midiblocks <midiblocks@gmail.com>
2020-10-27 23:08:14 -0700

    #98 Replaces prism with monaco, adds autoheight

--- src/pages/docs/handsfree/Index.vue
+++ src/pages/docs/handsfree/Index.vue
@@ -3,22 +3,21 @@ q-page.full-height
   section.content
     h1.text-center Handsfree.js
 
-    prism-editor.height-300(v-model='code.setup.npm' :highlight='highlighter' lineNumbers readonly)
+    .row.q-col-gutter-md.justify-center
+      .col-12.col-md-9
+        q-card
+          q-card-section
+            CodeEditor(:value='code.setup.npm' :options='options' :extraOptions='extraOptions')
 </template>
 
 <script>
-import {PrismEditor} from 'vue-prism-editor'
-import 'vue-prism-editor/dist/prismeditor.min.css'
-import {highlight, languages} from 'prismjs/components/prism-core'
-import 'prismjs/components/prism-clike'
-import 'prismjs/components/prism-javascript'
-import 'prismjs/components/prism-markup'
+import CodeEditor from '../../../components/CodeEditor'
 
 export default {
   name: 'DocsHandsfreeLanding',
 
   components: {
-    PrismEditor
+    CodeEditor
   },
 
   data: () => ({
@@ -26,16 +25,15 @@ export default {
       setup: {
         npm: require('!!raw-loader!./code/setup.npm.html').default,
       }
-    }
-  }),
+    },
 
-  methods: {
-    /**
-     * Highlights the passed code
-     */
-    highlighter (code) {
-      return highlight(code, languages.html)
+    options: {
+      readOnly: true
+    },
+    extraOptions: {
+      language: 'html',
+      autoheight: true
     }
-  }
+  })
 }
 </script>
