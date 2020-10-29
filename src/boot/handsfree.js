794b9aeb3a34f7339acc92f1db4d9613fb07a4a3
Midiblocks <midiblocks@gmail.com>
2020-10-28 21:16:45 -0700

    #99 Load calibration settings

--- src/boot/handsfree.js
+++ src/boot/handsfree.js
@@ -1,12 +1,19 @@
 import Vue from 'vue'
 import Handsfree from 'handsfree'
 import faceClick from './handsfree/faceClick'
+import store from 'store'
 
 // Instantiate
 const handsfree = window.handsfree = new Handsfree({
   assetsPath: document.location.origin + '/assets/',
   weboji: true
 })
+
+// Reset settings
+const calibration = store.get('handsfree.calibration', false)
+if (calibration) {
+  handsfree.plugin.facePointer.config = calibration
+}
 
 // Inject plugins
 handsfree.use('faceClick', faceClick)
