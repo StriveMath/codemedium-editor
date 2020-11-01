b2f52a2fc0fcdc2c341941ac6ef54a771d231a36
Midiblocks <midiblocks@gmail.com>
2020-11-01 15:31:22 -0800

    #104 Going further section, getToolbox is now a mixin

--- src/pages/StudioHome.vue
+++ src/pages/StudioHome.vue
@@ -72,6 +72,7 @@ import store from 'store'
 import Blockly from 'blockly'
 import toolbox from '../assets/toolboxes/studio'
 import {v4 as uuidv4} from 'uuid'
+import getToolbox from '../mixins/getToolbox'
 
 /**
  * @todo document
@@ -95,6 +96,8 @@ export default {
       }
     }
   },
+
+  mixins: [getToolbox],
 
   mounted () {
     set(window, 'app.$page', this)
@@ -194,7 +197,7 @@ export default {
       // Blockly options
       // @see https://developers.google.com/blockly/guides/configure/web/configuration_struct
       options: {},
-      toolbox: this.getToolbox(),
+      toolbox: this.getToolbox(toolbox),
 
       // Spliter width in pixels
       splitter: store.get('splitter', window.innerWidth / 3)
@@ -356,95 +359,6 @@ export default {
     checkBookmarks () {
       const bookmarks = this.$refs.workspace.blockly.getBlocksByType('bookmark')
       this.$store.commit('set', ['studio.hasBookmarks', !!bookmarks.length])
-    },
-
-    /**
-     * Gets an organized Blockly toolbox JSON, which consists of core blocks and custom blocks
-     */
-    getToolbox () {
-      const categories = cloneDeep(toolbox)
-      const customBlocks = store.get('blocks', {})
-
-      // Map categories to indexes
-      const coreBlockCats = categories.map(block => {
-        return block.category || ''
-      })
-
-      // Add custom block to appropriate category
-      Object.keys(customBlocks).forEach(id => {
-        const customBlock = customBlocks[id]
-        const catIndex = coreBlockCats.indexOf(customBlock.category)
-
-        if (categories[catIndex]) {
-          categories[catIndex].children.push({
-            tag: 'block',
-            type: customBlock.json.type
-          })
-
-          // Create block...
-          Blockly.Blocks[customBlock.json.type] = {
-            init: function() {
-              this.jsonInit(customBlock.json)
-            }
-          }
-          // ...and generator
-          Blockly.JavaScript[customBlock.json.type] = function (block) {
-            let code = []
-
-            customBlock.variables.forEach(variable => {
-              // Fields
-              if (variable.type === 'field') {
-                switch (variable.field) {
-                  case 'variable':
-                    code.push(`var $${variable.name} = ${JSON.stringify(Blockly.JavaScript.variableDB_.getName(block.getFieldValue(variable.name), Blockly.Variables.NAME_TYPE))}`)
-                  break
-                  case 'angle':
-                    code.push(`var $${variable.name} = ${block.getFieldValue(variable.name)}`)
-                  break
-                  case 'colour':
-                    code.push(`var $${variable.name} = ${JSON.stringify(block.getFieldValue(variable.name))}`)
-                  break
-                  case 'checkbox':
-                    code.push(`var $${variable.name} = ${block.getFieldValue(variable.name) === 'TRUE' ? 'true' : 'false'}`)
-                  break
-                  case 'dropdown':
-                    code.push(`var $${variable.name} = ${JSON.stringify(block.getFieldValue(variable.name))}`)
-                  break
-                  case 'number':
-                    code.push(`var $${variable.name} = ${block.getFieldValue(variable.name)}`)
-                  break
-                  case 'text':
-                    code.push(`var $${variable.name} = ${JSON.stringify(block.getFieldValue(variable.name))}`)
-                  break
-                }
-              // Inputs
-              } else {
-                switch (variable.input) {
-                  case 'value':
-                    code.push(`var $${variable.name} = ${JSON.stringify(Blockly.JavaScript.valueToCode(block, variable.name, Blockly.JavaScript.ORDER_ATOMIC))}`)
-                  break
-                  case 'statements':
-                    const statement = Blockly.JavaScript.statementToCode(block, variable.name)
-                    code.push(`var $${variable.name} = function () {\n${statement}\n}`)
-                  break
-                }
-              }
-            })
-            
-            code = code.join(';\n')
-            code += ';\n' + (customBlock.code || '')
-
-            // Return code
-            if (block.outputConnection) {
-              return [code, Blockly.JavaScript.ORDER_NONE]
-            } else {
-              return code
-            }
-          }
-        }
-      })
-
-      return categories
     }
   }
 }
